2b0f3e1fe871efcf5f109c8626f0a4add79077b3
Lukas Volf <jimm98y@hotmail.com>
2025-12-29 04:38:36 +0100

    F Updated cipher suites

--- src/SharpSRTP/SharpSRTP/DTLS/DtlsServer.cs
+++ src/SharpSRTP/SharpSRTP/DTLS/DtlsServer.cs
@@ -51,24 +51,32 @@ namespace SharpSRTP.DTLS
         {
             return new ProtocolVersion[]
             {
-                ProtocolVersion.DTLSv10,
-                ProtocolVersion.DTLSv12
+                // ProtocolVersion.DTLSv10,
+                ProtocolVersion.DTLSv12,
+                //ProtocolVersion.DTLSv13
             };
         }
 
         protected override int[] GetSupportedCipherSuites()
         {
             // TODO: review
+            // ECDSA certificates require matching cipher suites
             return new int[]
             {
+                // TLS 1.3 ciphers:
+                //CipherSuite.TLS_AES_256_GCM_SHA384,
+                //CipherSuite.TLS_AES_128_GCM_SHA256,
+
+                // TLS 1.2 ciphers:
                 CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
                 CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+
+                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384
             };
         }
 
