69972d78ace6843612f6a95b58f74bb3b339d0a8
Lukas Volf <jimm98y@hotmail.com>
2026-01-02 17:33:06 +0100

    T Consistency, refactoring

--- src/SharpSRTP.Tests/TestRFC5669.cs
+++ src/SharpSRTP.Tests/TestRFC5669.cs
@@ -36,12 +36,14 @@ namespace SharpSRTP.Tests
     public class TestRFC5669
     {
         // RFC 5669:
-        // [DataRow("0c5ffd37a11edc42c325287fc0604f2e", "f93563311b354748c978913795530631", "cd3a7c42c671e0067a2a2639b43a", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5ebdf5a89291e7e383e9beff765e691a73749c9e33139ad3001cd8da73ad07f69a2805a70358b5c7c8c60ed359f95cf5e08f713c53ff7b808250d79a19ccb8d10734e3cb72ed1f0a4e85b002b248049ab0763dbe571bec52cf9153fdf2019e421ef779cd6f4bd1c8211da8c272e2fce43934b9eabb87362510f254149f992599036f5e43102327db1ac5e78adc4f66546ed7abfb5a4db320fb7b9c52a61bc554e44a5cdaa4d9edc53763855")]
+        // [DataRow(SrtpCryptoSuites.SEED_CTR_128_HMAC_SHA1_80, "0c5ffd37a11edc42c325287fc0604f2e", "f93563311b354748c978913795530631", "cd3a7c42c671e0067a2a2639b43a", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5ebdf5a89291e7e383e9beff765e691a73749c9e33139ad3001cd8da73ad07f69a2805a70358b5c7c8c60ed359f95cf5e08f713c53ff7b808250d79a19ccb8d10734e3cb72ed1f0a4e85b002b248049ab0763dbe571bec52cf9153fdf2019e421ef779cd6f4bd1c8211da8c272e2fce43934b9eabb87362510f254149f992599036f5e43102327db1ac5e78adc4f66546ed7abfb5a4db320fb7b9c52a61bc554e44a5cdaa4d9edc53763855")]
         // Fixed:
-        [DataRow("0c5ffd37a11edc42c325287fc0604f2e", "f93563311b354748c978913795530631", "cd3a7c42c671e0067a2a2639b43a", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5ebdf5a89291e7e383e9beff765e691a73749c9e33139ad3001cd8da73ad07f69a2805a70358b5c7c8c60ed359f95cf5e08f713c53ff7b808250d79a19ccb8d10734e3cb72ed1f0a4e85b002b248049ab0763dbe571bec52cf9153fdf2019e421ef779cd6f4bd1c8211da8c272e2fce43934b9eabb87362510f254149f992599036f5e43102327db1ac5e78adc4f66546ed7abfb5a4db320fb7b9c52a61bc554e441d82cc2b73bb1517626c")]
+        [DataRow(SrtpCryptoSuites.SEED_CTR_128_HMAC_SHA1_80, "0c5ffd37a11edc42c325287fc0604f2e", "f93563311b354748c978913795530631", "cd3a7c42c671e0067a2a2639b43a", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5ebdf5a89291e7e383e9beff765e691a73749c9e33139ad3001cd8da73ad07f69a2805a70358b5c7c8c60ed359f95cf5e08f713c53ff7b808250d79a19ccb8d10734e3cb72ed1f0a4e85b002b248049ab0763dbe571bec52cf9153fdf2019e421ef779cd6f4bd1c8211da8c272e2fce43934b9eabb87362510f254149f992599036f5e43102327db1ac5e78adc4f66546ed7abfb5a4db320fb7b9c52a61bc554e441d82cc2b73bb1517626c")]
         [TestMethod]
-        public void Test_Encrypt_RTP_Seed_CTR(string key, string authKey, string salt, string rtp, string srtp)
+        public void Test_Encrypt_RTP_Seed_CTR(string cryptoSuite, string key, string authKey, string salt, string rtp, string srtp)
         {
+            SrtpProtectionProfileConfiguration protectionProfile = SrtpProtocol.SrtpCryptoSuites[cryptoSuite];
+
             byte[] k_e = Convert.FromHexString(key);
             byte[] k_a = Convert.FromHexString(authKey);
             byte[] k_s = Convert.FromHexString(salt);
@@ -56,7 +58,7 @@ namespace SharpSRTP.Tests
             int offset = RtpReader.ReadHeaderLen(payload);
 
             uint roc = 0;
-            ulong index = ((ulong)roc << 16) | sequenceNumber;
+            ulong index = SrtpContext.GenerateRtpIndex(roc, sequenceNumber);
             byte[] iv = CTR.GenerateMessageKeyIV(k_s, ssrc, index);
 
             var seed = new SeedEngine();
@@ -72,7 +74,7 @@ namespace SharpSRTP.Tests
             payload[length + 2] = (byte)(roc >> 8);
             payload[length + 3] = (byte)roc;
 
-            const int n_tag = 10;
+            int n_tag = protectionProfile.AuthTagLength >> 3;
 
             // It seems RFC 5669 has incorrect auth tag calculated from the original payload.
             // For a5cdaa4d9edc53763855 the following code works:
@@ -88,10 +90,12 @@ namespace SharpSRTP.Tests
             Assert.AreEqual(srtp, srtpResult);
         }
 
-        [DataRow("e91e5e75da65554a48181f3846349562", "0000000000000000000000000000", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5eb8a5363682c6b1bbf13c0b09cf747a5512543cb2f129b8bd0e92dfadf735cda8f88c4bbf90288f5e58d20c4f1bb0d58446ea009103ee57ba99cdeabaaa18d4a9a05ddb46e7e5290a5a2284fe50b1f6fe9ad3f1348c354181e85b24f1a552a1193cf0e13eed5ab95ae854fb4f5b0edb2d3ee5eb238c8f4bfb136b2eb6cd78760420680ce1879100014f140a15e07e70133ed9cbb6d57b75d574acb0087eefbac9936cd9ae602be3ee2cd8d5d9d")]
+        [DataRow(SrtpCryptoSuites.SEED_128_GCM_96, "e91e5e75da65554a48181f3846349562", "0000000000000000000000000000", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5eb8a5363682c6b1bbf13c0b09cf747a5512543cb2f129b8bd0e92dfadf735cda8f88c4bbf90288f5e58d20c4f1bb0d58446ea009103ee57ba99cdeabaaa18d4a9a05ddb46e7e5290a5a2284fe50b1f6fe9ad3f1348c354181e85b24f1a552a1193cf0e13eed5ab95ae854fb4f5b0edb2d3ee5eb238c8f4bfb136b2eb6cd78760420680ce1879100014f140a15e07e70133ed9cbb6d57b75d574acb0087eefbac9936cd9ae602be3ee2cd8d5d9d")]
         [TestMethod]
-        public void Test_Encrypt_RTP_Seed_GCM(string key, string salt, string rtp, string expectedEncryptedRTP)
+        public void Test_Encrypt_RTP_Seed_GCM(string cryptoSuite, string key, string salt, string rtp, string expectedEncryptedRTP)
         {
+            SrtpProtectionProfileConfiguration protectionProfile = SrtpProtocol.SrtpCryptoSuites[cryptoSuite];
+
             byte[] rtpBytes = Convert.FromHexString(rtp);
             byte[] k_e = Convert.FromHexString(key);
             byte[] k_s = Convert.FromHexString(salt); // salt must be zero!!!
@@ -99,7 +103,7 @@ namespace SharpSRTP.Tests
             uint ssrc = RtpReader.ReadSsrc(rtpBytes);
             ushort sequenceNumber = RtpReader.ReadSequenceNumber(rtpBytes);
             ulong index = SrtpContext.GenerateRtpIndex(0, sequenceNumber);
-            const int n_tag = 12;
+            int n_tag = protectionProfile.AuthTagLength >> 3;
 
             int offset = RtpReader.ReadHeaderLen(rtpBytes);
 
@@ -116,10 +120,12 @@ namespace SharpSRTP.Tests
             Assert.AreEqual(expectedEncryptedRTP, encryptedRTP);
         }
 
-        [DataRow("974bee725d44fc3992267b284c3c6750", "0000000000000000000000000000", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5eb486843a881df215a8574650ddabf5dbb2650f06f51252bccaeb4012899d6d71e30c64dad5ead5d8ba65ffe9d79aaf30dc9e6334490c07e7533d704114a9006ecb3b3bff59ecf585485bc0bd286ed434cfd684d19a1ad514ca5f37b71d93288c07cf4d5e9b83db8becc8c692a7279b6a9ac62ba970fc54f46dcc926d434c0b5ad8678fbf0e7a03037924dae342ef64fa65b8eaea260fecb477a57e3919c5dab82b0a8274cf6a8bb6cc466")]
+        [DataRow(SrtpCryptoSuites.SEED_128_CCM_80, "974bee725d44fc3992267b284c3c6750", "0000000000000000000000000000", "8008315ebf2e6fe020e8f5ebf57af5fd4ae19562976ec57a5a7ad55a5af5c5e5c5fdf5c55ad57a4a7272d57262e9729566ed66e97ac54a4a5a7ad5e15ae5fdd5fd5ac5d56ae56ad5c572d54ae54ac55a956afd6aed5a4ac562957a9516991691d572fd14e97ae962ed7a9f4a955af572e162f57a956666e17ae1f54a95f566d54a66e16e4afd6a9f7ae1c5c55ae5d56afde916c5e94a6ec56695e14afde1148416e94ad57ac5146ed59d1cc5", "8008315ebf2e6fe020e8f5eb486843a881df215a8574650ddabf5dbb2650f06f51252bccaeb4012899d6d71e30c64dad5ead5d8ba65ffe9d79aaf30dc9e6334490c07e7533d704114a9006ecb3b3bff59ecf585485bc0bd286ed434cfd684d19a1ad514ca5f37b71d93288c07cf4d5e9b83db8becc8c692a7279b6a9ac62ba970fc54f46dcc926d434c0b5ad8678fbf0e7a03037924dae342ef64fa65b8eaea260fecb477a57e3919c5dab82b0a8274cf6a8bb6cc466")]
         [TestMethod]
-        public void Test_Encrypt_RTP_Seed_CCM(string key, string salt, string rtp, string expectedEncryptedRTP)
+        public void Test_Encrypt_RTP_Seed_CCM(string cryptoSuite, string key, string salt, string rtp, string expectedEncryptedRTP)
         {
+            SrtpProtectionProfileConfiguration protectionProfile = SrtpProtocol.SrtpCryptoSuites[cryptoSuite];
+
             byte[] rtpBytes = Convert.FromHexString(rtp);
             byte[] k_e = Convert.FromHexString(key);
             byte[] k_s = Convert.FromHexString(salt); // salt must be zero!!!
@@ -127,7 +133,7 @@ namespace SharpSRTP.Tests
             uint ssrc = RtpReader.ReadSsrc(rtpBytes);
             ushort sequenceNumber = RtpReader.ReadSequenceNumber(rtpBytes);
             ulong index = SrtpContext.GenerateRtpIndex(0, sequenceNumber);
-            const int n_tag = 10;
+            int n_tag = protectionProfile.AuthTagLength >> 3;
 
             int offset = RtpReader.ReadHeaderLen(rtpBytes);
 
--- src/SharpSRTP.Tests/TestRFC7714.cs
+++ src/SharpSRTP.Tests/TestRFC7714.cs
@@ -21,6 +21,7 @@
 
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Modes;
+using SharpSRTP.DTLSSRTP;
 using SharpSRTP.SRTP;
 using SharpSRTP.SRTP.Encryption;
 using SharpSRTP.SRTP.Readers;
@@ -37,10 +38,10 @@ namespace Srtp.Tests
     {
         [DataRow("8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573", "517569642070726f2071756f", "51753c6580c2726f20718414")]
         [TestMethod]
-        public void Test_IV_RTP(string rtp, string salt, string expectedIv)
+        public void Test_IV_RTP(string rtp, string sk_s, string expectedIv)
         {
             byte[] rtpBytes = Convert.FromHexString(rtp);
-            byte[] k_s = Convert.FromHexString(salt);
+            byte[] k_s = Convert.FromHexString(sk_s);
 
             uint ssrc = RtpReader.ReadSsrc(rtpBytes);
             ushort sequenceNumber = RtpReader.ReadSequenceNumber(rtpBytes);
@@ -52,20 +53,21 @@ namespace Srtp.Tests
             Assert.AreEqual(expectedIv, ivString);
         }
 
-        [DataRow("8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573", "000102030405060708090a0b0c0d0e0f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b2f24de3a3fb34de6cacba861c9d7e4bcabe633bd50d294e6f42a5f47a51c7d19b36de3adf8833899d7f27beb16a9152cf765ee4390cce")]
-        [DataRow("8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b232b1de78a822fe12ef9f78fa332e33aab18012389a58e2f3b50b2a0276ffae0f1ba63799b87b7aa3db36dfffd6b0f9bb7878d7a76c13")]
+        [DataRow(ExtendedSrtpProtectionProfile.SRTP_AEAD_AES_128_GCM, "8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573", "000102030405060708090a0b0c0d0e0f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b2f24de3a3fb34de6cacba861c9d7e4bcabe633bd50d294e6f42a5f47a51c7d19b36de3adf8833899d7f27beb16a9152cf765ee4390cce")]
+        [DataRow(ExtendedSrtpProtectionProfile.SRTP_AEAD_AES_128_GCM, "8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b232b1de78a822fe12ef9f78fa332e33aab18012389a58e2f3b50b2a0276ffae0f1ba63799b87b7aa3db36dfffd6b0f9bb7878d7a76c13")]
         [TestMethod]
-        public void Test_Encrypt_RTP(string rtp, string key, string salt, string expectedEncryptedRTP)
+        public void Test_Encrypt_RTP(int dtlsProtectionProfile, string rtp, string sk_e, string sk_s, string expectedSrtp)
         {
+            SrtpProtectionProfileConfiguration protectionProfile = DtlsSrtpProtocol.DtlsProtectionProfiles[dtlsProtectionProfile];
+
             byte[] rtpBytes = Convert.FromHexString(rtp);
-            byte[] k_e = Convert.FromHexString(key);
-            byte[] k_s = Convert.FromHexString(salt);
+            byte[] k_e = Convert.FromHexString(sk_e);
+            byte[] k_s = Convert.FromHexString(sk_s);
 
             uint ssrc = RtpReader.ReadSsrc(rtpBytes);
             ushort sequenceNumber = RtpReader.ReadSequenceNumber(rtpBytes);
             ulong index = SrtpContext.GenerateRtpIndex(0, sequenceNumber);
-            const int n_tag = 16;
-
+            int n_tag = protectionProfile.AuthTagLength >> 3;
             int offset = RtpReader.ReadHeaderLen(rtpBytes);
 
             byte[] iv = AEAD.GenerateMessageKeyIV(k_s, ssrc, index);
@@ -78,23 +80,24 @@ namespace Srtp.Tests
             AEAD.Encrypt(cipher, result, offset, rtpBytes.Length, iv, k_e, n_tag, associatedData);
 
             string encryptedRTP = Convert.ToHexString(result).ToLowerInvariant();
-            Assert.AreEqual(expectedEncryptedRTP, encryptedRTP);
+            Assert.AreEqual(expectedSrtp, encryptedRTP);
         }
 
-        [DataRow("8040f17b8041f8d35501a0b2f24de3a3fb34de6cacba861c9d7e4bcabe633bd50d294e6f42a5f47a51c7d19b36de3adf8833899d7f27beb16a9152cf765ee4390cce", "000102030405060708090a0b0c0d0e0f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573")]
-        [DataRow("8040f17b8041f8d35501a0b232b1de78a822fe12ef9f78fa332e33aab18012389a58e2f3b50b2a0276ffae0f1ba63799b87b7aa3db36dfffd6b0f9bb7878d7a76c13", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573")]
+        [DataRow(ExtendedSrtpProtectionProfile.SRTP_AEAD_AES_128_GCM, "8040f17b8041f8d35501a0b2f24de3a3fb34de6cacba861c9d7e4bcabe633bd50d294e6f42a5f47a51c7d19b36de3adf8833899d7f27beb16a9152cf765ee4390cce", "000102030405060708090a0b0c0d0e0f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573")]
+        [DataRow(ExtendedSrtpProtectionProfile.SRTP_AEAD_AES_128_GCM, "8040f17b8041f8d35501a0b232b1de78a822fe12ef9f78fa332e33aab18012389a58e2f3b50b2a0276ffae0f1ba63799b87b7aa3db36dfffd6b0f9bb7878d7a76c13", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "517569642070726f2071756f", "8040f17b8041f8d35501a0b247616c6c696120657374206f6d6e69732064697669736120696e207061727465732074726573")]
         [TestMethod]
-        public void Test_Decrypt_RTP(string srtp, string key, string salt, string expectedDecryptedRTP)
+        public void Test_Decrypt_RTP(int dtlsProtectionProfile, string srtp, string sk_e, string sk_s, string expectedRtp)
         {
+            SrtpProtectionProfileConfiguration protectionProfile = DtlsSrtpProtocol.DtlsProtectionProfiles[dtlsProtectionProfile];
+
             byte[] srtpBytes = Convert.FromHexString(srtp);
-            byte[] k_e = Convert.FromHexString(key);
-            byte[] k_s = Convert.FromHexString(salt);
+            byte[] k_e = Convert.FromHexString(sk_e);
+            byte[] k_s = Convert.FromHexString(sk_s);
 
             uint ssrc = RtpReader.ReadSsrc(srtpBytes);
             ushort sequenceNumber = RtpReader.ReadSequenceNumber(srtpBytes);
             ulong index = SrtpContext.GenerateRtpIndex(0, sequenceNumber);
-            const int n_tag = 16;
-
+            int n_tag = protectionProfile.AuthTagLength >> 3;
             int offset = RtpReader.ReadHeaderLen(srtpBytes);
 
             byte[] iv = AEAD.GenerateMessageKeyIV(k_s, ssrc, index);
@@ -103,82 +106,82 @@ namespace Srtp.Tests
             byte[] associatedData = srtpBytes.Take(offset).ToArray();
             AEAD.Encrypt(cipher, srtpBytes, offset, srtpBytes.Length - n_tag, iv, k_e, n_tag, associatedData);
 
-            string decryptedRTP = Convert.ToHexString(srtpBytes.Take(srtpBytes.Length - n_tag).ToArray()).ToLowerInvariant();
-            Assert.AreEqual(expectedDecryptedRTP, decryptedRTP);
+            string result = Convert.ToHexString(srtpBytes.Take(srtpBytes.Length - n_tag).ToArray()).ToLowerInvariant();
+            Assert.AreEqual(expectedRtp, result);
         }
 
         [DataRow("81c8000e4d6172734e5450314e545031525450200000042a0000eb984c756e61deadbeefdeadbeefdeadbeefdeadbeefdeadbeef", "517569642070726f2071756f", (uint)0x000005d4, "517524055203726f207170bb")]
         [TestMethod]
-        public void Test_IV_RTCP(string rtcp, string salt, uint index, string expectedIv)
+        public void Test_IV_RTCP(string rtcp, string sk_s, uint index, string expectedIv)
         {
             byte[] rtpBytes = Convert.FromHexString(rtcp);
-            byte[] k_s = Convert.FromHexString(salt);
-
+            byte[] k_s = Convert.FromHexString(sk_s);
             uint ssrc = RtcpReader.ReadSsrc(rtpBytes);
-            byte[] iv = AEAD.GenerateMessageKeyIV(k_s, ssrc, index);
 
+            byte[] iv = AEAD.GenerateMessageKeyIV(k_s, ssrc, index);
             string ivString = Convert.ToHexString(iv).ToLowerInvariant();
             Assert.AreEqual(expectedIv, ivString);
         }
 
-        [DataRow("81c8000d4d6172734e5450314e545032525450200000042a0000e9304c756e61deadbeefdeadbeefdeadbeefdeadbeefdeadbeef", "000102030405060708090a0b0c0d0e0f", "517569642070726f2071756f", (uint)0x000005d4, "81c8000d4d61727363e94885dcdab67ca727d7662f6b7e997ff5c0f76c06f32dc676a5f1730d6fda4ce09b4686303ded0bb9275bc84aa45896cf4d2fc5abf87245d9eade800005d4")]
+        [DataRow(ExtendedSrtpProtectionProfile.SRTP_AEAD_AES_128_GCM, "81c8000d4d6172734e5450314e545032525450200000042a0000e9304c756e61deadbeefdeadbeefdeadbeefdeadbeefdeadbeef", "000102030405060708090a0b0c0d0e0f", "517569642070726f2071756f", (uint)0x000005d4, "81c8000d4d61727363e94885dcdab67ca727d7662f6b7e997ff5c0f76c06f32dc676a5f1730d6fda4ce09b4686303ded0bb9275bc84aa45896cf4d2fc5abf87245d9eade800005d4")]
         [TestMethod]
-        public void Test_Encrypt_RTCP(string rtcp, string key, string salt, uint idx, string expectedSRTCP)
+        public void Test_Encrypt_RTCP(int dtlsProtectionProfile, string rtcp, string sk_e, string sk_s, uint idx, string expectedSrtcp)
         {
+            SrtpProtectionProfileConfiguration protectionProfile = DtlsSrtpProtocol.DtlsProtectionProfiles[dtlsProtectionProfile];
+
             byte[] rtcpBytes = Convert.FromHexString(rtcp);
-            byte[] k_e = Convert.FromHexString(key);
-            byte[] k_s = Convert.FromHexString(salt);
+            byte[] k_e = Convert.FromHexString(sk_e);
+            byte[] k_s = Convert.FromHexString(sk_s);
             uint ssrc = RtcpReader.ReadSsrc(rtcpBytes);
 
             int offset = RtcpReader.GetHeaderLen();
             byte[] iv = AEAD.GenerateMessageKeyIV(k_s, ssrc, idx);
 
-            const int n_tag = 16;
-            byte[] result = new byte[rtcpBytes.Length + n_tag + 4];
-            Buffer.BlockCopy(rtcpBytes, 0, result, 0, rtcpBytes.Length);
+            int n_tag = protectionProfile.AuthTagLength >> 3;
+            byte[] srtcp = new byte[rtcpBytes.Length + n_tag + 4];
+            Buffer.BlockCopy(rtcpBytes, 0, srtcp, 0, rtcpBytes.Length);
 
             var cipher = new GcmBlockCipher(new AesEngine());
-
             const uint E_FLAG = 0x80000000;
             uint index = idx | E_FLAG;
-            byte[] associatedData = result.Take(offset).Concat(new byte[] { (byte)(index >> 24), (byte)(index >> 16), (byte)(index >> 8), (byte)index }).ToArray(); // associatedData include also index
-            AEAD.Encrypt(cipher, result, offset, rtcpBytes.Length, iv, k_e, n_tag, associatedData);
+            byte[] associatedData = srtcp.Take(offset).Concat(new byte[] { (byte)(index >> 24), (byte)(index >> 16), (byte)(index >> 8), (byte)index }).ToArray(); // associatedData include also index
+            AEAD.Encrypt(cipher, srtcp, offset, rtcpBytes.Length, iv, k_e, n_tag, associatedData);
 
-            result[rtcpBytes.Length + n_tag + 0] = (byte)(index >> 24);
-            result[rtcpBytes.Length + n_tag + 1] = (byte)(index >> 16);
-            result[rtcpBytes.Length + n_tag + 2] = (byte)(index >> 8);
-            result[rtcpBytes.Length + n_tag + 3] = (byte)index;
+            srtcp[rtcpBytes.Length + n_tag + 0] = (byte)(index >> 24);
+            srtcp[rtcpBytes.Length + n_tag + 1] = (byte)(index >> 16);
+            srtcp[rtcpBytes.Length + n_tag + 2] = (byte)(index >> 8);
+            srtcp[rtcpBytes.Length + n_tag + 3] = (byte)index;
 
-            string encryptedRTCP = Convert.ToHexString(result).ToLowerInvariant();
-            Assert.AreEqual(expectedSRTCP, encryptedRTCP);
+            string result = Convert.ToHexString(srtcp).ToLowerInvariant();
+            Assert.AreEqual(expectedSrtcp, result);
         }
 
-        [DataRow("81c8000d4d617273d50ae4d1f5ce5d304ba297e47d470c282c3ece5dbffe0a50a2eaa5c1110555be8415f658c61de0476f1b6fad1d1eb30c4446839f57ff6f6cb26ac3be800005d4", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "517569642070726f2071756f", "81c8000d4d6172734e5450314e545032525450200000042a0000e9304c756e61deadbeefdeadbeefdeadbeefdeadbeefdeadbeef")]
+        [DataRow(ExtendedSrtpProtectionProfile.SRTP_AEAD_AES_128_GCM, "81c8000d4d617273d50ae4d1f5ce5d304ba297e47d470c282c3ece5dbffe0a50a2eaa5c1110555be8415f658c61de0476f1b6fad1d1eb30c4446839f57ff6f6cb26ac3be800005d4", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "517569642070726f2071756f", "81c8000d4d6172734e5450314e545032525450200000042a0000e9304c756e61deadbeefdeadbeefdeadbeefdeadbeefdeadbeef")]
         [TestMethod]
-        public void Test_Decrypt_RTCP(string srtcp, string key, string salt, string expectedRTCP)
+        public void Test_Decrypt_RTCP(int dtlsProtectionProfile, string srtcp, string sk_e, string sk_s, string expectedRtcp)
         {
+            SrtpProtectionProfileConfiguration protectionProfile = DtlsSrtpProtocol.DtlsProtectionProfiles[dtlsProtectionProfile];
+
             byte[] srtcpBytes = Convert.FromHexString(srtcp);
-            byte[] k_e = Convert.FromHexString(key);
-            byte[] k_s = Convert.FromHexString(salt);
+            byte[] k_e = Convert.FromHexString(sk_e);
+            byte[] k_s = Convert.FromHexString(sk_s);
 
-            const int n_tag = 16;
-
+            int n_tag = protectionProfile.AuthTagLength >> 3;
             uint ssrc = RtcpReader.ReadSsrc(srtcpBytes);
             uint idx = RtcpReader.SrtcpReadIndex(srtcpBytes, 0);
             
             const uint E_FLAG = 0x80000000;
             uint index = idx & ~E_FLAG;
-
             int offset = RtcpReader.GetHeaderLen();
+
             byte[] iv = AEAD.GenerateMessageKeyIV(k_s, ssrc, index);
 
             var cipher = new GcmBlockCipher(new AesEngine());
-
             byte[] associatedData = srtcpBytes.Take(offset).Concat(srtcpBytes.Skip(srtcpBytes.Length - 4).Take(4)).ToArray(); // associatedData include also index
             AEAD.Encrypt(cipher, srtcpBytes, offset, srtcpBytes.Length - 4 - n_tag, iv, k_e, n_tag, associatedData);
 
-            string decryptedRTCP = Convert.ToHexString(srtcpBytes.Take(srtcpBytes.Length - 4 - n_tag).ToArray()).ToLowerInvariant();
-            Assert.AreEqual(expectedRTCP, decryptedRTCP);
+            string result = Convert.ToHexString(srtcpBytes.Take(srtcpBytes.Length - 4 - n_tag).ToArray()).ToLowerInvariant();
+            Assert.AreEqual(expectedRtcp, result);
         }
     }
 }
