1a44f87108347232dc2a19e5d0f4b5d22b4f5b4a
Lukas Volf <jimm98y@hotmail.com>
2026-01-04 15:11:02 +0100

    B Fixed DTLS server offering cipher suites that don't match the certificate

--- src/SharpSRTP/DTLS/DtlsServer.cs
+++ src/SharpSRTP/DTLS/DtlsServer.cs
@@ -93,26 +93,40 @@ namespace SharpSRTP.DTLS
 
         protected override int[] GetSupportedCipherSuites()
         {
-            // ECDSA certificates require matching cipher suites
-            return new int[]
+            if (CertificateSignatureAlgorithm == SignatureAlgorithm.rsa)
             {
-                // TLS 1.3 ciphers:
-                //CipherSuite.TLS_AES_256_GCM_SHA384,
-                //CipherSuite.TLS_AES_128_GCM_SHA256,
-                //CipherSuite.TLS_CHACHA20_POLY1305_SHA256,
+                return new int[]
+                {
+                    // TLS 1.2 ciphers:
+                    CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                    CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+                    CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+                    CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384,
+                    CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+                };
+            }
+            else if (CertificateSignatureAlgorithm == SignatureAlgorithm.ecdsa)
+            {
+                // ECDSA certificates require matching cipher suites
+                return new int[]
+                {
+                    // TLS 1.3 ciphers:
+                    //CipherSuite.TLS_AES_256_GCM_SHA384,
+                    //CipherSuite.TLS_AES_128_GCM_SHA256,
+                    //CipherSuite.TLS_CHACHA20_POLY1305_SHA256,
 
-                // TLS 1.2 ciphers:
-                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384,
-                CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,
-                CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-            };
+                    // TLS 1.2 ciphers:
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+                };
+            }
+            else
+            {
+                throw new InvalidOperationException($"DTLS server certificate algorithm {CertificateSignatureAlgorithm} not supported!");
+            }
         }
 
         public virtual DtlsTransport DoHandshake(out string handshakeError, DatagramTransport datagramTransport, Func<string> getRemoteEndpoint, Func<string, DatagramTransport> createClientDatagramTransport)
