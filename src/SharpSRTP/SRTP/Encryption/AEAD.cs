ed9b5d94de0930012032764cdad094cf12266897
Lukas Volf <jimm98y@hotmail.com>
2026-02-08 21:15:40 +0100

    B Removed Expected length check #2

--- src/SharpSRTP/SRTP/Encryption/AEAD.cs
+++ src/SharpSRTP/SRTP/Encryption/AEAD.cs
@@ -31,12 +31,6 @@ namespace SharpSRTP.SRTP.Encryption
         {
             int payloadSize = length - offset;
 
-            int expectedLength = engine.GetOutputSize(payloadSize);
-            if (offset + expectedLength > payload.Length)
-            {
-                throw new ArgumentOutOfRangeException("Payload is too small!");
-            }
-
             var parameters = new AeadParameters(new KeyParameter(K_e), N_tag << 3, iv, associatedData);
             engine.Init(encrypt, parameters);
 
